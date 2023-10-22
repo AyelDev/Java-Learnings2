c88a7eeecc7b27fbe9f8c8886cc7d14def65987f
AyelDev <aarielabelgas23@gmail.com>
2023-10-22 16:02:03 +0800

    updates for functional programming

--- Functional-Programming/src/com/FunctionAsParameter/FunctionalProgramming.java
+++ Functional-Programming/src/com/FunctionAsParameter/FunctionalProgramming.java
@@ -1,5 +1,5 @@
 package com.FunctionAsParameter;
 
 public class FunctionalProgramming {
-//
+
 }
--- Functional-Programming/src/com/FunctionAsParameter/FunctionalProgrammingRunner.java
+++ Functional-Programming/src/com/FunctionAsParameter/FunctionalProgrammingRunner.java
@@ -6,16 +6,22 @@ public class FunctionalProgrammingRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<String> list = List.of("sad", "happy", "joy", "alone");
-		printWithFp(list);
+//		List<String> list = List.of("sad", "happy", "joy", "alone");
+//		printWithFp(list);
+		
+		List<Number> numlist = List.of(1.4,23f,3,4);
+		printWithFp(numlist);
+		
+		
 	}
 
-	private static void printBasic(List<String> list) {
-		for(String string: list) {
-			System.out.println(string);
-		}
-	}
-	private static void printWithFp(List<String> list) {
+//	private static void printBasic(List<String> list) {
+//		for(String string: list) {
+//			System.out.println(string);
+//		}
+//	}
+	
+	private static void printWithFp(List<Number> list) {
 		list.stream().forEach(
 					element -> System.out.println("element : " + element)
 				);
